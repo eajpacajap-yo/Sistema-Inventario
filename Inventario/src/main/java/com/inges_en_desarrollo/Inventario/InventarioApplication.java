e60d64007ea8734151e1a3e4c380a947c8d7c969
Edgar Josias Ajpacaja Poncio <eajpacajap@miumg.edu.gt>
2026-09-06 23:26:42 -0600

    Creación automática de usuario admin al iniciar

--- Inventario/src/main/java/com/inges_en_desarrollo/Inventario/InventarioApplication.java
+++ Inventario/src/main/java/com/inges_en_desarrollo/Inventario/InventarioApplication.java
@@ -1,13 +1,31 @@
 package com.inges_en_desarrollo.Inventario;
 
+import com.inges_en_desarrollo.Inventario.model.usuario;
+import com.inges_en_desarrollo.Inventario.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventarioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventarioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner initDatabase(UsuarioRepository usuarioRepository) {
+        return args -> {
+            if (!usuarioRepository.findByEmail("admin@correo.com").isPresent()) {
+                usuario admin = new usuario();
+                admin.setEmail("admin@correo.com");
+                admin.setPassword("admin123");
+                admin.setNombre("Administrador");
+                
+                usuarioRepository.save(admin);
+                System.out.println(">>> USUARIO ADMIN CREADO EXITOSAMENTE <<<");
+            }
+        };
+    }
 }
